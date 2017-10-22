1cc601aa37fba8ac121f7f9cb57ca0f4488177a4
vjspranav <pranavasri@live.in>
2017-10-22 10:52:56 +0000

    y

--- device.mk
+++ device.mk
@@ -257,7 +257,7 @@ PRODUCT_PACKAGES += \
     libQWiFiSoftApCfg \
     libwpa_client \
     hostapd \
-    dhcpcd.conf \
+    #dhcpcd.conf \
     wpa_supplicant \
     wpa_supplicant.conf \
     wcnss_service
